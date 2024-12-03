962d44ae8aece2a2260af497a91e304f3ad2bd7c
MrXie82193-hub <68369300+MrXie82193-hub@users.noreply.github.com>
2024-12-03 15:45:27 +0800

    Add files via upload

---
+++ effect2.js
@@ -0,0 +1,27 @@
+function heartWith(body) {
+    this.body = body;
+    this.init = function(){
+        this.monitor();
+    };
+    this.monitor = function () {
+        var that = this;
+        document.onmousemove = function (event) {
+            var heart = document.createElement("div");
+            heart.className = "heart-with";
+            var x = event.clientX, y = event.clientY;
+            // 设置heart跟随鼠标的位置出现
+            heart.style.left = x + "px", heart.style.top = y + "px";
+            // 设置heart的大小随机 Math.random()返回一个0~1之间的数
+            var size = Math.random() * 30;
+            heart.style.width = size + "px", heart.style.height = size + "px";
+            // 将heart添加到body下
+            that.body.appendChild(heart);
+            // 设置定时器，两秒钟情理一次heart
+            setInterval(function () {
+                heart.remove();
+            }, 2000);
+        }
+    };
+
+    this.init();
+}

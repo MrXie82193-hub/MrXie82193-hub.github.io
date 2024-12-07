f80e84e9144430ccc932761e8fbfc66d22d9069b
MrXie82193-hub <68369300+MrXie82193-hub@users.noreply.github.com>
2024-12-07 19:02:22 +0800

    Update effect2.js

--- effect2.js
+++ effect2.js
@@ -6,20 +6,23 @@ function heartWith(body) {
     this.monitor = function () {
         var that = this;
         document.onmousemove = function (event) {
-            var heart = document.createElement("div");
-            heart.className = "heart-with";
-            var x = event.pageX, y = event.pageY;
-            // 设置heart跟随鼠标的位置出现
-            heart.style.left = x + "px", heart.style.top = y + "px";
-            // 设置heart的大小随机 Math.random()返回一个0~1之间的数
-            var size = Math.random() * 30;
-            heart.style.width = size + "px", heart.style.height = size + "px";
-            // 将heart添加到body下
-            that.body.appendChild(heart);
-            // 设置定时器，两秒钟情理一次heart
-            setInterval(function () {
-                heart.remove();
-            }, 2000);
+			var pageheight = Math.max(document.body.scrollHeight,document.documentElement.scrollHeight);
+			if(event.pageY <= pageheight * 0.98) {
+				var heart = document.createElement("div");
+				heart.className = "heart-with";
+				var x = event.pageX, y = event.pageY;
+				// 设置heart跟随鼠标的位置出现
+				heart.style.left = x + "px", heart.style.top = y + "px";
+				// 设置heart的大小随机 Math.random()返回一个0~1之间的数
+				var size = Math.random() * 30;
+				heart.style.width = size + "px", heart.style.height = size + "px";
+				// 将heart添加到body下
+				that.body.appendChild(heart);
+				// 设置定时器，两秒钟情理一次heart
+				setInterval(function () {
+					heart.remove();
+				}, 2000);
+			}
         }
     };
     this.init();
